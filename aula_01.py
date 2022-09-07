b43dc138ab0bc5f92e79bdd9d29e96ce51f1f7b8
Daniel Wzoreck <wzoreck2000@gmail.com>
2022-09-07 00:33:06 -0300

    Aula 02 - Normalizacao e exemplo em nova base de dados

--- aula_01.py
+++ aula_01.py
@@ -81,3 +81,131 @@ scaler_credit = MinMaxScaler()
 
 X_credit = scaler_credit.fit_transform(X_credit) # Normalização dos dados previsores
 
+
+#### Outra basa de dados -------------------------------------------------------------------------------
+
+# Exploração dos dados
+base_census = pd.read_csv('census.csv')
+
+base_census.describe()
+
+base_census.isnull().sum()
+
+# Visualização dos dados
+np.unique(base_census['income'], return_counts=True)
+
+sns.countplot(x=base_census['income'])
+
+plt.hist(x=base_census['age'])
+
+plt.hist(base_census['education-num'])
+
+plt.hist(base_census['hour-per-week'])
+
+import plotly.express as px
+
+grafico = px.treemap(base_census, path=['workclass'])
+
+grafico.write_html('grafico0.html')
+
+grafico = px.treemap(base_census, path=['workclass', 'age'])
+
+grafico.write_html('grafico1.html')
+
+grafico = px.treemap(base_census, path=['occupation', 'relationship', 'age'])
+
+grafico.write_html('grafico2.html')
+
+    # Categorias paralelas
+grafico = px.parallel_categories(base_census, dimensions=['occupation', 'relationship'])
+
+grafico.write_html('grafico3.html')
+
+grafico = px.parallel_categories(base_census, dimensions=['native-country', 'income'])
+
+grafico.write_html('grafico4.html')
+
+grafico = px.parallel_categories(base_census, dimensions=['workclass', 'occupation', 'income'])
+
+grafico.write_html('grafico5.html')
+
+grafico = px.parallel_categories(base_census, dimensions=['education', 'income'])
+
+grafico.write_html('grafico6.html')
+
+    # Divisao Previsores e Classes
+base_census.columns
+
+X_census = base_census.iloc[:, 0:14].values
+
+Y_census = base_census.iloc[:, 14].values
+
+    # Tratamento de atributos categóricos
+    ## LABELENCODER - converte string para inteiro ex: P M G -> 0 1 2
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
+
+indices = [1, 3, 5, 6, 7, 8, 9, 13]
+
+for i in indices:
+    X_census[:, i] = label_encoder.fit_transform(X_census[:, i])
+    
+    ## ONEHOTENCODER
+    # EX Carro - Gol Pálio Uno
+    
+    # No LABELENCODER seria
+    # Gol Pálio Uno
+    # 0   1     2
+    
+    # No ONEHOTENCODER
+    # Gol   1 0 0
+    #Pálio  0 1 0
+    #Uno    0 0 1
+    
+    # Não há peso por ser apenas 0 e 1 (há algoritmos que isso interfere)
+    
+len(np.unique(base_census['workclass']))
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+onehotencoder_census = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), indices)], remainder='passthrough')
+
+X_census = onehotencoder_census.fit_transform(X_census).toarray()
+
+X_census.shape # Qtd registros e colunas
+
+# Escalonamento de valores
+
+from sklearn.preprocessing import StandardScaler
+
+scaler_census = StandardScaler()
+
+X_census = scaler_census.fit_transform(X_census)
+
+# Divisão bases Treinamento e Teste
+from sklearn.model_selection import train_test_split
+
+X_census_treinamento, X_census_teste, Y_census_treinamento, Y_census_teste = train_test_split(X_census, Y_census, test_size=0.25, random_state=0)
+
+
+X_credit_treinamento, X_credit_teste, Y_credit_treinamento, Y_credit_teste = train_test_split(X_credit, Y_credit, test_size=0.25, random_state=0)
+
+# Salvar variáveis
+import pickle
+
+with open('census.pkl', mode='wb') as f:
+    pickle.dump([X_census_treinamento, X_census_teste, Y_census_treinamento, Y_census_teste], f)
+    
+with open('credit.pkl', mode='wb') as f:
+    pickle.dump([X_credit_treinamento, X_credit_teste, Y_credit_treinamento, Y_credit_teste], f)
+
+
+    
+
+
+
+
+
+
